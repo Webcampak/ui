2d30182859c39b90308b9d8cef07fabf42291cf6
Fgerthoffert <francois@webcampak.com>
2016-10-18 23:14:31 -0400

    re-arranged some sections of the configuration window

---
+++ Sencha/App6.0/workspace/Desktop/app/view/sourcesconfiguration/ftp/local/Main.js
@@ -0,0 +1,17 @@
+//<debug>
+console.log(new Date().toLocaleTimeString() + ': Log: Load: WPAKD.view.sourcesconfiguration.ftp.local.Main');
+//</debug>
+Ext.define('WPAKD.view.sourcesconfiguration.ftp.local.Main', {
+    extend: 'Ext.form.FieldSet'
+    , alias: 'widget.sourcesconfigurationftplocalmain'
+
+    , title: i18n.gettext('Local FTP Account')
+
+    , layout: 'fit'
+    , defaults: {labelWidth: 200}
+    , items: [
+        {xtype: 'sourcesconfigurationftplocalcfglocalftpusername'}
+        , {xtype: 'sourcesconfigurationftplocalcfglocalftppass'}
+    ]
+});
+
