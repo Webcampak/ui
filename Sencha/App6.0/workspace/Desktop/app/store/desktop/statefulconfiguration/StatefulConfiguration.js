6e382eee211d9a439681dbb5cb219c83863b88d0
Fgerthoffert <francois@webcampak.com>
2016-10-23 11:02:10 -0400

    Fixed style errors reported by codacy

--- Sencha/App6.0/workspace/Desktop/app/store/desktop/statefulconfiguration/StatefulConfiguration.js
+++ Sencha/App6.0/workspace/Desktop/app/store/desktop/statefulconfiguration/StatefulConfiguration.js
@@ -3,48 +3,36 @@
 console.log(new Date().toLocaleTimeString() + ": Log: Load: WPAKD.store.desktop.statefulconfiguration.StatefulConfiguration");
 //</debug>
 Ext.define("WPAKD.store.desktop.statefulconfiguration.StatefulConfiguration", {
-    extend: "Ext.data.Store",
+    extend: "Ext.data.Store"
 
-    model: "WPAKD.model.desktop.statefulconfiguration.StatefulConfiguration",
+    , model: "WPAKD.model.desktop.statefulconfiguration.StatefulConfiguration"
 
 //    autoLoad: true,
-    autoSync: true,
+    , autoSync: true
 
-    proxy:{
-        type: "direct",
-        extraParams: {
+    , proxy:{
+        type: "direct"
+        , extraParams: {
             WIDGET: "null"
             , SENCHA_APP: "WPAKD"
-        },
-        reader: {
-            type: "json",
-            rootProperty: "results",
-            totalProperty: "total"
-        },
-        api:{
-            read:      "DesktopStatefulConfiguration.getStatefulConfiguration",
-            create:    "DesktopStatefulConfiguration.addUpdateStatefulConfiguration",
-            destroy:   "DesktopStatefulConfiguration.removeStatefulConfiguration",
-            update:    "DesktopStatefulConfiguration.addUpdateStatefulConfiguration"
-        }/*,
-        afterRequest:function(request,success){
-            if (success === false) {
-                if (request.operation.error) {var errorMsg = request.operation.error;}
-                else {var errorMsg = "Unknown Error";}
-                Ext.MessageBox.show({
-                    title: "Error",
-                    msg: errorMsg,
-                    buttons: Ext.MessageBox.OK,
-                    icon: Ext.MessageBox.ERROR
-                });
-            }
-        }*/
-    },
-    listeners: {
-        beforeload: function(store, opt){
+        }
+        , reader: {
+            type: "json"
+            , rootProperty: "results"
+            , totalProperty: "total"
+        }
+        , api:{
+            read:      "DesktopStatefulConfiguration.getStatefulConfiguration"
+            , create:    "DesktopStatefulConfiguration.addUpdateStatefulConfiguration"
+            , destroy:   "DesktopStatefulConfiguration.removeStatefulConfiguration"
+            , update:    "DesktopStatefulConfiguration.addUpdateStatefulConfiguration"
+        }
+    }
+    , listeners: {
+        beforeload: function(){
             Ext.getBody().mask("Please wait, loading content...");
-        },
-        dataChanged: function(store) {
+        }
+        , dataChanged: function() {
             Ext.getBody().unmask();
         }
     }
