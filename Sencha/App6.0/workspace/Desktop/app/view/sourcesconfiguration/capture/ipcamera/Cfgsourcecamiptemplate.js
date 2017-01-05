161082e07fe5556d7457a581d0960c6f86ceac8c
fgerthoffert <francois@webcampak.com>
2017-01-05 12:55:30 -0500

    Added support for tplink cameras

--- Sencha/App6.0/workspace/Desktop/app/view/sourcesconfiguration/capture/ipcamera/Cfgsourcecamiptemplate.js
+++ Sencha/App6.0/workspace/Desktop/app/view/sourcesconfiguration/capture/ipcamera/Cfgsourcecamiptemplate.js
@@ -18,11 +18,12 @@ Ext.define("WPAKD.view.sourcesconfiguration.capture.ipcamera.Cfgsourcecamiptempl
     , store: Ext.create("Ext.data.Store", {
                 fields : ["name", "value"]
                 , data   : [
-                    {name : i18n.gettext("Date when file was save into /tmp/ directory"),               value: "filedate"       }
-                    , {name : i18n.gettext("Picture filename (Webcampak) - (YYYYMMDDHHMMSS.jpg)"),      value: "webcampak"      }
-                    , {name : i18n.gettext("Picture filename (Harbortronics) - (YYYYMMDD_HHMMSS.JPG)"), value: "harbortronics"  }
+                    {name : i18n.gettext("Date when file was save into /tmp/ directory"),                           value: "filedate"       }
+                    , {name : i18n.gettext("Picture filename (Webcampak) - (YYYYMMDDHHMMSS.jpg)"),                  value: "webcampak"      }
+                    , {name : i18n.gettext("Picture filename (Harbortronics) - (YYYYMMDD_HHMMSS.JPG)"),             value: "harbortronics"  }
+                    , {name : i18n.gettext("Picture filename (Harbortronics) - (IP_01_YYYYMMDDHHMMSS_TIMING.JPG)"), value: "tplink"         }
                     //, {name : i18n.gettext("Picture filename (Canon IP Camera) - (YYYYMMDDHHMMSSxxx.jpg)"),          value: "canonipcamera"  },
-                    , {name : i18n.gettext("EXIF Metadata (date)"),                                     value: "exif"           }
+                    , {name : i18n.gettext("EXIF Metadata (date)"),                                                 value: "exif"           }
                 ]
             })
     , listeners : {
