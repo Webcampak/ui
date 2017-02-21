8a597cba5d9490983dfed6697370e8ec3594af94
Francois G <f.gerthoffert@gmail.com>
2017-02-21 18:07:06 -0500

    Update Cfgwatermarkpositionx.js

--- Sencha/App6.0/workspace/Desktop/app/view/sourcesconfiguration/postprod/watermark/Cfgwatermarkpositionx.js
+++ Sencha/App6.0/workspace/Desktop/app/view/sourcesconfiguration/postprod/watermark/Cfgwatermarkpositionx.js
@@ -6,7 +6,6 @@ Ext.define("WPAKD.view.sourcesconfiguration.postprod.watermark.Cfgwatermarkposit
     extend: "Ext.form.field.Number"
     , alias: "widget.sourcesconfigurationpostprodwatermarkcfgwatermarkpositionx"
 
-    , fieldLabel: i18n.gettext("X")
     , allowBlank: false
     , minValue:0
     , listeners : {
